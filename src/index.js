aa1e88f03ef2ddac0f0718f77db01d1c2c2f0567
Luca Di Molfetta <63596667+dimolf345@users.noreply.github.com>
2021-09-23 22:14:23 +0200

    Add files via upload
    first two sections almost completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // import * as bootstrap from "bootstrap";
+"use strict";
+
+let toggler = document.getElementById("navbarToggler");
+let navbar = document.getElementById("mainNav");
+
+const toggleAriaExp = (string) => (string === "false" ? "true" : "false");
+
+toggler.addEventListener("click", function () {
+  toggler.setAttribute(
+    "aria-expanded",
+    toggleAriaExp(this.attributes["aria-expanded"].textContent)
+  );
+  navbar.classList.toggle("expanded");
+});
